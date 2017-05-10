27b49d15b688bcf47ad16a3a0f82015cdbbdabf8
A1rPun <thombakker@gmail.com>
2017-05-10 23:35:30 +0200

    :+1: Fix vuejs application

--- www/js/index.js
+++ www/js/index.js
@@ -1,40 +1,35 @@
-var $ = function (q) { return document.querySelector(q); }; // jQuery one-liner (joke)
-var app = {
-    decks: {
-        standard: [0, 0.5, 1, 2, 3, 5, 8, 13, 20, 40, 100],
-        fibonacci: [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89],
-        natural: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-        tshirt: ['XXS', 'XS', 'S', 'M', 'L', 'XL', 'XXL'],
-        misc: ['?', '&#8734;', '&#9749;']
-    },
-    menu: ['Standard', 'Fibonacci', 'Natural', 'T-Shirt'],    
-    cardClick: function (event) {
-        var el = event.currentTarget;
-        el.style.position = 'absolute';
-        el.style.left = 0;
-        el.style.top = 0;
-        el.style.right = 0;
-        el.style.bottom = 0;
-    },
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-        this.currentDeck = this.decks.standard.slice().concat(this.decks.misc.slice());
-    },
-    onDeviceReady: function () { },
-};
-
-app.initialize();
-
-var a1 = new Vue({
-    el: '#navigation-container',
+var scrubPoker = new Vue({
+    el: '#ScrubPoker',
     data: {
-        menu: app.menu
-    }
-});
-var a2 = new Vue({
-    el: '#cards-container',
-    data: {
-        cardClick: app.cardClick,
-        currentDeck: app.currentDeck
+        decks: [{
+            name: 'Standard',
+            deck: [0, '½', 1, 2, 3, 5, 8, 13, 20, 40, 100]
+        }, {
+            name: 'Fibonacci',
+            deck: [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+        }, {
+            name: 'Natural',
+            deck: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+        }, {
+            name: 'T-Shirt',
+            deck: ['XXS', 'XS', 'S', 'M', 'L', 'XL', 'XXL']
+        }, {
+            name: 'Misc',
+            deck: ['?', '&#8734;', '&#9749;']
+        }],
+        currentDeck: [],
+    },
+    methods: {
+        cardClick: function (event) { },
+        deckClick: function (event, deck) {
+            this.setDeck(deck.deck);
+        },
+        setDeck: function (deck) {
+            var deck2 = this.decks[this.decks.length - 1].deck.slice();
+            this.currentDeck = deck.slice().concat(deck2);
+        },
+    },
+    created: function () {
+        this.setDeck(this.decks[0].deck);
     }
 });
