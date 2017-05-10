3ebadb8a868ee9cbb4409fd618d94d24a6052131
A1rPun <thombakker@gmail.com>
2017-05-10 22:35:11 +0200

    :v: Add vuejs templates

--- www/js/index.js
+++ www/js/index.js
@@ -1,37 +1,40 @@
-var $ = function (q) { return document.querySelector(q); };
-var decks = {
-    standard: [0, 0.5, 1, 2, 3, 5, 8, 13, 20, 40, 100],
-    fibonacci: [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89],
-    natural: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-    tshirt: ['XXS', 'XS', 'S', 'M', 'L', 'XL', 'XXL'],
-    misc: ['?', '&#8734;', '&#9749;']
-};
+var $ = function (q) { return document.querySelector(q); }; // jQuery one-liner (joke)
 var app = {
+    decks: {
+        standard: [0, 0.5, 1, 2, 3, 5, 8, 13, 20, 40, 100],
+        fibonacci: [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89],
+        natural: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+        tshirt: ['XXS', 'XS', 'S', 'M', 'L', 'XL', 'XXL'],
+        misc: ['?', '&#8734;', '&#9749;']
+    },
+    menu: ['Standard', 'Fibonacci', 'Natural', 'T-Shirt'],    
+    cardClick: function (event) {
+        var el = event.currentTarget;
+        el.style.position = 'absolute';
+        el.style.left = 0;
+        el.style.top = 0;
+        el.style.right = 0;
+        el.style.bottom = 0;
+    },
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-        var cc = $('#cards-container');
-        var ct = $('#cards-template').innerHTML;
-        cc.innerHTML = '';
-        this.createCards(cc, ct, decks.standard);
-        this.createCards(cc, ct, decks.misc);
-    },
-    createCards: function (cc, template, cards) {        
-        for (var i = 0, l = cards.length; i < l; i++) {
-            var zz = document.createElement('div');
-            zz.innerHTML = template;
-            zz.querySelector('.mdl-card').innerHTML = cards[i];
-            zz.addEventListener('click', this.cardClick);
-            cc.appendChild(zz.firstElementChild);
-        }
-    },
-    cardClick: function () {
-        this.style.position = 'absolute';
-        this.style.left = 0;
-        this.style.top = 0;
-        this.style.right = 0;
-        this.style.bottom = 0;
+        this.currentDeck = this.decks.standard.slice().concat(this.decks.misc.slice());
     },
     onDeviceReady: function () { },
 };
 
 app.initialize();
+
+var a1 = new Vue({
+    el: '#navigation-container',
+    data: {
+        menu: app.menu
+    }
+});
+var a2 = new Vue({
+    el: '#cards-container',
+    data: {
+        cardClick: app.cardClick,
+        currentDeck: app.currentDeck
+    }
+});
